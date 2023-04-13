c097467dae253b04573aaf356dea4e0e74d9ca72
cardon <philantrope1@outlook.com>
2023-04-13 09:55:06 +0200

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'chat', to: 'message#chat'
+  resources :messages, only: [:create]
+  get 'chat', to: 'messages#chat'
   # Defines the root path route ("/")
   # root "articles#index"
 end
