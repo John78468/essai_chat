03215eaff4722fba23c61fb14f3521caecd2830d
cardon <philantrope1@outlook.com>
2023-04-13 09:45:46 +0200

    controller Message add params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,13 +5,18 @@ class MessagesController < ApplicationController
 
 
   def create
-    @message = Message.new
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to chat_path
+    else
+      render 'chat'
+    end
   end
 
 
   private
 
   def message_params
-
+    params.require(:message).permit(:content)
   end
 end
